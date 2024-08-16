64cce2c363ac63a4829cda45a1cf25c73607e2af
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-16 10:35:56 +0200

    creati nel runner altre cards del cane

--- card/src/main/java/salvatore/assennato/card/CardRunner.java
+++ card/src/main/java/salvatore/assennato/card/CardRunner.java
@@ -42,10 +42,20 @@ public class CardRunner implements CommandLineRunner {
     }
     public void createCards(){
         // 1)Creazione Cards Cani
-        Card cardCane1 = new Card("https://plus.unsplash.com/premium_photo-1668208365386-4198381c6f6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2hpYmElMjBpbnV8ZW58MHx8MHx8fDA%3D","Sbiba inu","Bellissimo Shiba inu,adatto a tutti itipi di famiglie", TipoAnimale.Cane, Taglia.Piccola);
-        cardDAO.save(cardCane1);
+          //Sezione Taglia Piccola
+          //Card cardCane1 = new Card("https://plus.unsplash.com/premium_photo-1668208365386-4198381c6f6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2hpYmElMjBpbnV8ZW58MHx8MHx8fDA%3D","Sbiba inu","Bellissimo Shiba inu,adatto a tutti itipi di famiglie", TipoAnimale.Cane, Taglia.Piccola);
+          //cardDAO.save(cardCane1);
+
+          //Sezione Taglia Media
+          //Card cardCaneMedio1 = new Card("https://images.unsplash.com/photo-1667080799488-3fa8fe75cdd8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvdyUyMGNob3d8ZW58MHx8MHx8fDA%3D","Chow chow","bellissimo cane Chow chow",TipoAnimale.Cane,Taglia.Media);
+          //cardDAO.save(cardCaneMedio1);
+
+          //Sezione Taglia Grande
+          //Card cardCaneGrande1 = new Card("https://images.unsplash.com/photo-1651137540084-684834335af9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8U2FuJTIwQmVybmFyZG98ZW58MHx8MHx8fDA%3D","San Bernardo","bellissimo e coccoloso San Bernardo",TipoAnimale.Cane,Taglia.Grande);
+          //cardDAO.save(cardCaneGrande1);
+
         // 2)Creazione Cards gatti
-        Card  cardGatto1 = new Card("https://media.istockphoto.com/id/1814865780/it/foto/un-adorabile-gatto-american-curl-sta-fissando-la-telecamera.jpg?s=1024x1024&w=is&k=20&c=uiTF9uZc6pmbMviPHALb6ZtwGZPE4zEnjlqZKLMRjZM=","American Curl","bellissimo esemplare di American Curl,coccoloso con tutti",TipoAnimale.Gatto,Taglia.Media);
-        cardDAO.save(cardGatto1);
+        //Card cardGatto1 = new Card("https://media.istockphoto.com/id/1814865780/it/foto/un-adorabile-gatto-american-curl-sta-fissando-la-telecamera.jpg?s=1024x1024&w=is&k=20&c=uiTF9uZc6pmbMviPHALb6ZtwGZPE4zEnjlqZKLMRjZM=","American Curl","bellissimo esemplare di American Curl,coccoloso con tutti",TipoAnimale.Gatto,Taglia.Media);
+        //cardDAO.save(cardGatto1);
     }
 }
