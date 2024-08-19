a9e37639a2989081bc690c8fdd7d11927901e4f5
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-19 09:51:02 +0200

    aggiunto il CrossOrigin con il frontend con Angular

--- card/src/main/java/salvatore/assennato/card/controllers/CardController.java
+++ card/src/main/java/salvatore/assennato/card/controllers/CardController.java
@@ -1,6 +1,7 @@
 package salvatore.assennato.card.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import salvatore.assennato.card.service.CardService;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/cards")
 public class CardController {
     @Autowired
