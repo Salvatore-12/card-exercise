f5cd67327a86c9dcf03f0b41842a6ec1c3b58832
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-09-11 11:21:04 +0200

    creazione card degli uccelli sul cardRunner

--- card/src/main/java/salvatore/assennato/card/CardRunner.java
+++ card/src/main/java/salvatore/assennato/card/CardRunner.java
@@ -43,26 +43,37 @@ public class CardRunner implements CommandLineRunner {
     public void createCards(){
         // 1)Creazione Cards Cani
           //Sezione Taglia Piccola
-          Card cardCane1 = new Card("https://plus.unsplash.com/premium_photo-1668208365386-4198381c6f6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2hpYmElMjBpbnV8ZW58MHx8MHx8fDA%3D","Sbiba inu","Bellissimo Shiba inu,adatto a tutti itipi di famiglie", TipoAnimale.Cane, Taglia.Piccola);
-          cardDAO.save(cardCane1);
+            Card cardCane1 = new Card("https://plus.unsplash.com/premium_photo-1668208365386-4198381c6f6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2hpYmElMjBpbnV8ZW58MHx8MHx8fDA%3D","Sbiba inu","Bellissimo Shiba inu,adatto a tutti itipi di famiglie", TipoAnimale.Cane, Taglia.Piccola);
+            cardDAO.save(cardCane1);
 
           //Sezione Taglia Media
-          Card cardCaneMedio1 = new Card("https://images.unsplash.com/photo-1667080799488-3fa8fe75cdd8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvdyUyMGNob3d8ZW58MHx8MHx8fDA%3D","Chow chow","bellissimo cane Chow chow",TipoAnimale.Cane,Taglia.Media);
-          cardDAO.save(cardCaneMedio1);
+            Card cardCaneMedio1 = new Card("https://images.unsplash.com/photo-1667080799488-3fa8fe75cdd8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvdyUyMGNob3d8ZW58MHx8MHx8fDA%3D","Chow chow","bellissimo cane Chow chow",TipoAnimale.Cane,Taglia.Media);
+            cardDAO.save(cardCaneMedio1);
 
           //Sezione Taglia Grande
-          Card cardCaneGrande1 = new Card("https://images.unsplash.com/photo-1651137540084-684834335af9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8U2FuJTIwQmVybmFyZG98ZW58MHx8MHx8fDA%3D","San Bernardo","bellissimo e coccoloso San Bernardo",TipoAnimale.Cane,Taglia.Grande);
-          cardDAO.save(cardCaneGrande1);
+             Card cardCaneGrande1 = new Card("https://images.unsplash.com/photo-1651137540084-684834335af9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8U2FuJTIwQmVybmFyZG98ZW58MHx8MHx8fDA%3D","San Bernardo","bellissimo e coccoloso San Bernardo",TipoAnimale.Cane,Taglia.Grande);
+             cardDAO.save(cardCaneGrande1);
 
         // 2)Creazione Cards Gatti
            //Sezione Taglia Piccola
-           Card cardGattoPiccolo1 = new Card("https://images.unsplash.com/photo-1693778132696-19a993323880?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29ybmlzaCUyMHJleHxlbnwwfHwwfHx8MA%3D%3D","cornish-rex","bellissimo gattino",TipoAnimale.Gatto,Taglia.Piccola);
-           cardDAO.save(cardGattoPiccolo1);
+             Card cardGattoPiccolo1 = new Card("https://images.unsplash.com/photo-1693778132696-19a993323880?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29ybmlzaCUyMHJleHxlbnwwfHwwfHx8MA%3D%3D","cornish-rex","bellissimo gattino",TipoAnimale.Gatto,Taglia.Piccola);
+             cardDAO.save(cardGattoPiccolo1);
            //Sezione Taglia Media
-           Card cardGatto1 = new Card("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQM7A4PEIA1VKTHryXPShQeujrayN6SddBtQ&s","American Curl","bellissimo esemplare di American Curl,coccoloso con tutti",TipoAnimale.Gatto,Taglia.Media);
-           cardDAO.save(cardGatto1);
+             Card cardGatto1 = new Card("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQM7A4PEIA1VKTHryXPShQeujrayN6SddBtQ&s","American Curl","bellissimo esemplare di American Curl,coccoloso con tutti",TipoAnimale.Gatto,Taglia.Media);
+             cardDAO.save(cardGatto1);
            //Sezione Taglia Grande
-           Card cardGattoGrande1 = new Card("https://images.unsplash.com/photo-1510932309205-50f8913fdb7c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TWFpbmUlMjBDb29ufGVufDB8fDB8fHww","Maine Coon","bellissimo gatto pelosetto",TipoAnimale.Gatto,Taglia.Grande);
-           cardDAO.save(cardGattoGrande1);
+             Card cardGattoGrande1 = new Card("https://images.unsplash.com/photo-1510932309205-50f8913fdb7c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TWFpbmUlMjBDb29ufGVufDB8fDB8fHww","Maine Coon","bellissimo gatto pelosetto",TipoAnimale.Gatto,Taglia.Grande);
+             cardDAO.save(cardGattoGrande1);
+
+        // 2)Creazione Cards Uccelli
+             //Sezione Taglia Piccola
+             Card cardUcceloPiccolo = new Card("https://images.unsplash.com/photo-1599481182960-c1ec9a3c1db8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWwlMjBwYXNzZXJvJTIwY29tdW5lfGVufDB8fDB8fHww","Il passero comune","bellissimo esemplare di passero comune",TipoAnimale.Uccello,Taglia.Piccola);
+             cardDAO.save(cardUcceloPiccolo);
+             //Sezione Taglia Media
+             Card cardUcceloMedia = new Card("https://images.unsplash.com/photo-1583606638017-8d10ae9b3c91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWNxdWlsb25lJTIwbmVybyUyMHVjY2VsbG98ZW58MHx8MHx8fDA%3D","Aquilone nero","bellissimo esemplare di un aquilone nero",TipoAnimale.Uccello,Taglia.Media);
+             cardDAO.save(cardUcceloMedia);
+             //Sezione Taglia Piccola
+             Card cardUcceloGrande = new Card("https://plus.unsplash.com/premium_photo-1664302704305-1ad71a6d84ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXF1aWxhJTIwYXJwaWF8ZW58MHx8MHx8fDA%3D","Aquila arpia","bellissimo esemplare di aquila arpia",TipoAnimale.Uccello,Taglia.Grande);
+             cardDAO.save(cardUcceloGrande);
     }
 }
